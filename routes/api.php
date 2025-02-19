9e8e8a713ac4320d12677fcb9dd666b3ada72fd8
Kyle Metivier <metivier.kyle@yahoo.com>
2025-02-19 13:09:46 -0600

    Update api.php
    group with alias

--- routes/api.php
+++ routes/api.php
@@ -13,32 +13,61 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\ApiTokenController;
 use App\Http\Middleware\CheckApiKey;
+use App\Http\Middleware\UserAccess;
+use App\Http\Middleware\AdminAccess;
 
-Route::resource('admin', AdminController::class)->except(['create', 'edit'])->middleware(CheckApiKey::class);
+//Admin Routes
+Route::middleware(['token_check', 'admin'])->group(function () {
+    Route::get("admin/all", [AdminController::class, 'index']);
+    Route::get("admin/", [AdminController::class, 'show']);
+    Route::post("admin/", [AdminController::class, 'store']);
+    Route::put("admin/", [AdminController::class, 'update']);
+    Route::delete("admin/", [AdminController::class, 'destroy']);
+    Route::get("admin/cart/all", [CartController::class, 'index']);
+    Route::resource('token', ApiTokenController::class)->except(['create', 'edit']);
+    Route::post("product/", [ProductController::class, 'store']);
+    Route::put("product/{product_id}", [ProductController::class, 'update']);
+    Route::delete("product/{product_id}", [ProductController::class, 'destory']);
+    Route::delete("photo/{photo_id}", [PhotoController::class, 'destory']);
+    Route::resource('category', CategoryController::class)->except(['create', 'edit', 'update']);
+    Route::get("category/{categoryId}/products", [CategoryController::class, 'products']);
+});
 
-Route::resource('token', ApiTokenController::class)->except(['create', 'edit'])->middleware(CheckApiKey::class);
+//User Routes
+Route::middleware(['token_check', 'user'])->group(function () {
+    Route::get("user/", [UserController::class, 'show']);
+    Route::put("user/", [UserController::class, 'update']);
+    Route::delete("user/", [UserController::class, 'destroy']);
+    Route::get("user/shipping", [UserController::class, 'getShippingInfo']);
+    Route::get("user/cart", [UserController::class, 'getCartInfo']);
+    Route::get("user/totalSale", [UserController::class, 'totalSales']);
+    Route::get("user/orders", [UserController::class, 'getOrders']);
+    Route::get("cart/session/{session_id}", [CartController::class, 'cartBySession']);
+    Route::get("cart/user/", [CartController::class, 'cartByUser']);
+    Route::resource('cart', CartController::class)->except(['create', 'edit']);
+    Route::get("order/user/", [OrderController::class, 'orderByUser']);
+    Route::resource('order', OrderController::class)->except(['create', 'edit']);
+    Route::resource('shipping', ShippingController::class)->except(['create', 'edit']);
+});
 
-Route::get("cart/session/{session_id}", [CartController::class, 'cartBySession'])->middleware(CheckApiKey::class);
-Route::get("cart/user/{user_id}", [CartController::class, 'cartByUser'])->middleware(CheckApiKey::class);
-Route::resource('cart', CartController::class)->except(['create', 'edit'])->middleware(CheckApiKey::class);
+//Global Routes
+Route::middleware('token_check')->group(function () {
+    Route::post("user/login", [UserController::class, 'login']);
+    Route::post("admin/login", [AdminController::class, 'login']);
+    Route::post("user/", [UserController::class, 'store']);
+    Route::get("product/featured", [ProductController::class, 'featured']);
+    Route::get("product/available", [ProductController::class, 'available']);
+    Route::get("product/{productId}", [ProductController::class, 'available']);
+    Route::get("product/search/{search}", [ProductController::class, 'search']);
+    Route::resource('photo', PhotoController::class)->except(['create', 'edit', 'update', 'index']);
+});
 
-Route::get("order/user/{user_id}", [OrderController::class, 'orderByUser'])->middleware(CheckApiKey::class);
-Route::resource('order', OrderController::class)->except(['create', 'edit'])->middleware(CheckApiKey::class);
 
-Route::get("product/featured", [ProductController::class, 'featured'])->middleware(CheckApiKey::class);
-Route::get("product/available", [ProductController::class, 'available'])->middleware(CheckApiKey::class);
-Route::get("product/search/{search}", [ProductController::class, 'search'])->middleware(CheckApiKey::class);
-Route::resource('product', ProductController::class)->except(['create', 'edit'])->middleware(CheckApiKey::class);
 
-Route::resource('photo', PhotoController::class)->except(['create', 'edit', 'update'])->middleware(CheckApiKey::class);
 
-Route::resource('category', CategoryController::class)->except(['create', 'edit', 'update'])->middleware(CheckApiKey::class);
-Route::get("category/{categoryId}/products", [CategoryController::class, 'products'])->middleware(CheckApiKey::class);
-Route::resource('shipping', ShippingController::class)->except(['create', 'edit'])->middleware(CheckApiKey::class);
 
-Route::get("user/{userId}/shipping", [UserController::class, 'getShippingInfo'])->middleware(CheckApiKey::class);
-Route::get("user/{userId}/cart", [UserController::class, 'getCartInfo'])->middleware(CheckApiKey::class);
-Route::get("user/{userId}/totalSale", [UserController::class, 'totalSales'])->middleware(CheckApiKey::class);
-Route::get("user/{userId}/orders", [UserController::class, 'getOrders'])->middleware(CheckApiKey::class);
-Route::resource('user', UserController::class)->except(['create', 'edit'])->middleware(CheckApiKey::class);
+
+
+
+
 
