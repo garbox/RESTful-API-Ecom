0b9e9752fef7f5d37ceb6a92052f8d3612ba429a
Kyle Metivier <metivier.kyle@yahoo.com>
2025-02-19 13:09:30 -0600

    Update AppServiceProvider.php
    removed scramble

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
-use Dedoc\Scramble\Scramble;
 use Illuminate\Routing\Route;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,11 +24,6 @@ class AppServiceProvider extends ServiceProvider
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
-        });
-
-        Scramble::configure()
-        ->routes(function (Route $route) {
-            return Str::startsWith($route->uri, 'api/');
         });
     }
 }
