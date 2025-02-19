c5526bacd5a192e24e2ac6665d507237b3608aa7
Kyle Metivier <metivier.kyle@yahoo.com>
2025-02-19 13:09:19 -0600

    Middleware alias
    Middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Exceptions\InvalidOrderException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,17 +17,16 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
-
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'user' => \App\Http\Middleware\UserAccess::class,
+            'admin' => \App\Http\Middleware\AdminAccess::class,
+            'token_check' => \App\Http\Middleware\CheckApiKey::class,
         ]);
-
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
-
-        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    })
+    ->create();
