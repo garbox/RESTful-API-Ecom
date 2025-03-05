e6de6b5a928a54574ab00b5346a94e34c78da8a9
Kyle Metivier <metivier.kyle@yahoo.com>
2025-03-04 18:24:13 -0600

    Update Views

--- resources/views/components/category-crud.blade.php
+++ resources/views/components/category-crud.blade.php
@@ -1,15 +1,154 @@
 <h5 data-toggle="collapse" href="#CategoryMain" role="button" aria-expanded="false" aria-controls="CategoryMain" style="cursor: pointer;">Category Type</h5>
 <ul class="collapse" id="CategoryMain">
     <!-- GET /category -->
-    <li>GET /category</li>
+    <li>
+        <span data-toggle="collapse" href="#categoryIndex" role="button" aria-expanded="false" aria-controls="categoryIndex" style="cursor: pointer;">
+        GET {{parse_url(route('category.index'),PHP_URL_PATH)}}
+        </span>
+        <div class="collapse codeAera" id="categoryIndex">
+            <code>
+            <p>Request<br>json</p>
+            headers: {<br>
+            "GLOBAL-API-KEY" : string,<br>
+            "USER_API_KEY": string/Admin<br>
+            }
+            <hr>
+            <p>Response<br>json</p>
+            [
+            {<br>
+                "id": integer,<br>
+                "name":string,<br>
+                "created_at":date ,<br>
+                "updated_at": date<br>
+            }]
+            </code>
+        </div>
+    </li>
     <!-- GET /category/{category_id} -->
-    <li>GET /category/{category_id}</li>
+    <li>
+        <span data-toggle="collapse" href="#categoryShow" role="button" aria-expanded="false" aria-controls="categoryShow" style="cursor: pointer;">
+        GET api/category/{category_id}
+        </span>
+        <div class="collapse codeAera" id="categoryShow">
+            <code>
+            <p>Request<br>json</p>
+            headers: {<br>
+            "GLOBAL-API-KEY" : string,<br>
+            "USER_API_KEY": string/Admin<br>
+            }
+            <hr>
+            <p>Response<br>json</p>
+            {<br>
+                "id": integer,<br>
+                "name":string,<br>
+                "created_at":date ,<br>
+                "updated_at": date<br>
+            }
+            </code>
+        </div>
+    </li>
     <!-- GET /category/{category_id}/products -->
-    <li>GET /category/{category_id}/products</li>
+    <li>
+        <span data-toggle="collapse" href="#categoryProducts" role="button" aria-expanded="false" aria-controls="categoryProducts" style="cursor: pointer;">
+        GET api/category/{categoryId}/products
+        </span>
+        <div class="collapse codeAera" id="categoryProducts">
+            <code>
+            <p>Request<br>json</p>
+            headers: {<br>
+            "GLOBAL-API-KEY" : string,<br>
+            "USER_API_KEY": string/Admin<br>
+            }
+            <hr>
+            <p>Response<br>json</p>
+            [{ <br>
+                "id": integer, <br>
+                "name": string, <br>
+                "short_description": string, <br>
+                "price": integer, <br>
+                "category_id": integer, <br>
+                "long_description": string, <br>
+                "featured": integer, <br>
+                "available": integer, <br>
+                "created_at": date, <br>
+                "updated_at": date <br>
+            }]
+            </code>
+        </div>
+    </li>
     <!-- PUT /category/{category_id} -->
-    <li>PUT /category/{category_id}</li>
+    <li>
+        <span data-toggle="collapse" href="#categoryUpdate" role="button" aria-expanded="false" aria-controls="categoryUpdate" style="cursor: pointer;">
+        PUT api/category/{category_id}
+        </span>
+        <div class="collapse codeAera" id="categoryUpdate">
+            <code>
+            <p>Request<br>json</p>
+            headers: {<br>
+            "GLOBAL-API-KEY" : string,<br>
+            "USER_API_KEY": string/Admin<br>
+            }<br>
+            body: {<br>
+            "name":string<br>
+            }
+            <hr>
+            <p>Response<br>json</p>
+            {<br>
+                "id": integer,<br>
+                "name":string,<br>
+                "created_at":date ,<br>
+                "updated_at": date<br>
+            }
+            </code>
+        </div>
+    </li>
     <!-- POST /category -->
-    <li>POST /category</li>
+    <li>
+        <span data-toggle="collapse" href="#categoryStore" role="button" aria-expanded="false" aria-controls="categoryStore" style="cursor: pointer;">
+        POST {{parse_url(route('category.store'),PHP_URL_PATH)}}
+        </span>
+        <div class="collapse codeAera" id="categoryStore">
+            <code>
+            <p>Request<br>json</p>
+            headers: {<br>
+            "GLOBAL-API-KEY" : string,<br>
+            "USER_API_KEY": string/Admin<br>
+            }<br>
+            body: {<br>
+            "name":string<br>
+            }
+            <hr>
+            <p>Response<br>json</p>
+            {<br>
+                "id": integer,<br>
+                "name":string,<br>
+                "created_at":date ,<br>
+                "updated_at": date<br>
+            }
+            </code>
+        </div>
+    </li>
     <!-- DELETE /category/{category_id} -->
-    <li>DELETE /category/{category_id}</li>
+    <li>
+        <span data-toggle="collapse" href="#categoryDestroy" role="button" aria-expanded="false" aria-controls="categoryDestroy" style="cursor: pointer;">
+        DELETE /category/{category_id}
+        </span>
+        <div class="collapse codeAera" id="categoryDestroy">
+            <code>
+            <p>Request<br>json</p>
+            headers: {<br>
+            "GLOBAL-API-KEY" : string,<br>
+            "USER_API_KEY": string/Admin<br>
+            }<br>
+            body: {<br>
+            "id": integer<br>
+            }
+            <hr>
+            <p>Response<br>json</p>
+            {<br>
+                'message' : 'Category was deleted successfully.'
+            }
+            </code>
+        </div>
+    </li>
 </ul>
--- resources/views/components/order-crud.blade.php
+++ resources/views/components/order-crud.blade.php
@@ -2,15 +2,114 @@
 <ul class="collapse" id="orderMain">
     
     <!-- GET /order/{order} -->
-    <li>GET /order/{order}</li>
+    <li>
+        <span data-toggle="collapse" href="#adminOrderShow" role="button" aria-expanded="false" aria-controls="adminOrderShow" style="cursor: pointer;">
+            GET api/order/{order_id}
+            </span>
+
+            <div class="collapse codeAera" id="adminOrderShow" >
+                <code> 
+                <p>REQUEST:<br>json</p>
+                headers: {<br>
+                "GLOBAL-API-KEY" : string,<br>
+                "USER-API-KEY": string<br>
+                }<br>
+                <hr>
+                <p>RESPONSE:<br>json</p>
+                {<br>
+                'id' : integer,<br>
+                'user_id' : integer,<br>
+                'stripe_payment_id' : string,<br>
+                'total_price' : integer<br>
+                }
+                </code>
+            </div>
+    </li>
 
     <!-- POST /order -->
-    <li>POST /order</li>
+     <!-- Needs shipping info, and must be placed by a user -->
+    <li>
+        <span data-toggle="collapse" href="#orderStore" role="button" aria-expanded="false" aria-controls="orderStore" style="cursor: pointer;">
+            POST {{parse_url(route('order.create'),PHP_URL_PATH)}}
+            </span>
+
+            <div class="collapse codeAera" id="orderStore" >
+                <code> 
+                <p>REQUEST:<br>json</p>
+                headers: {<br>
+                "GLOBAL-API-KEY" : string,<br>
+                "USER-API-KEY": string<br>
+                }<br>
+                body: {<br>
+                'shipping_name' : string,<br>
+                'shipping_email' : string,<br>
+                'shipping_phone' : string,<br>
+                'shipping_address' : string,<br>
+                'shipping_city' : string,<br>
+                'shipping_state' : string,<br>
+                'shipping_zip' : string,<br>
+                }
+                <hr>
+                <p>RESPONSE:<br>json</p>
+                {<br>
+                'id' : integer,<br>
+                'user_id' : integer,<br>
+                'stripe_payment_id' : string,<br>
+                'total_price' : integer<br>
+                }
+                </code>
+            </div>
+    </li>
 
     <!-- PUT /order/{order_id} -->
-    <li>PUT /order/{order_id}</li>
+    <li>
+        <span data-toggle="collapse" href="#orderUpdate" role="button" aria-expanded="false" aria-controls="orderUpdate" style="cursor: pointer;">
+            PUT api/order/
+            </span>
+            <div class="collapse codeAera" id="orderUpdate" >
+                <code> 
+                <p>REQUEST:<br>json</p>
+                headers: {<br>
+                "GLOBAL-API-KEY" : string,<br>
+                "USER-API-KEY": string<br>
+                }<br>
+                body: {<br>
+                    'order_id' : integer,<br>
+                    'stripe_payment_intent_id' : integer<br>
+                }
+                <hr>
+                <p>RESPONSE:<br>json</p>
+                {<br>
+                    'id' : integer<br>
+                    'user_id' : integer<br>
+                    'total_price' : integer<br>
+                    'stripe_payment_intent_id' : string <br>
+                }
+                </code>
+            </div>
+    </li>
 
     <!-- DELETE /order/{order_id}-->
-    <li>DELETE /order/{order_id}</li>
+    <li>
+        <span data-toggle="collapse" href="#orderDelete" role="button" aria-expanded="false" aria-controls="orderDelete" style="cursor: pointer;">
+            DELETE api/order/{order_id}
+            </span>
+
+            <div class="collapse codeAera" id="orderDelete" >
+                <code> 
+                <p>REQUEST:<br>json</p>
+                headers: {<br>
+                "GLOBAL-API-KEY" : string,<br>
+                "USER-API-KEY": string<br>
+                }
+                <hr>
+                <p>RESPONSE:<br>json</p>
+                {<br>
+                    'message' : 'Order deleted successfully.'<br>
+                }
+                </code>
+            </div>
+    </li>
+        </li>
 
 </ul>
