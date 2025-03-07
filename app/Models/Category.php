55d53ff66ae01a60edad5263f804e8d26e91cd26
Kyle Metivier <metivier.kyle@yahoo.com>
2025-03-07 16:59:39 -0600

    Update Category.php
    clean up header

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+
 
 class Category extends Model
 {
