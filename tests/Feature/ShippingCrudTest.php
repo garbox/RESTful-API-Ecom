1be204c0744bc47b91b99ac7adcf13d2cc41fc5b
Kyle Metivier <metivier.kyle@yahoo.com>
2025-03-14 16:59:39 -0500

    Update ShippingCrudTest.php
    added headers, route names, and clean up

--- tests/Feature/ShippingCrudTest.php
+++ tests/Feature/ShippingCrudTest.php
@@ -4,10 +4,13 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Shipping;
 use App\Models\Order;
+use App\Models\Admin;
+use App\Models\ApiToken;
 
 class ShippingCrudTest extends TestCase
 {
@@ -16,8 +19,15 @@ class ShippingCrudTest extends TestCase
 
     /** @test */
     public function it_can_create_a_shipping_info(){
+        Artisan::call('app:admin-api-token');
         $user = User::factory()->create();
         $order = Order::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
+        $headers = [
+            'GLOBAL-API-KEY' => $token,
+            'USER-API-KEY' => $user->api_token,
+        ];
+
         $shippingData = [
             'name' => $this->faker->name,  
             'email' => $this->faker->unique()->safeEmail,  
@@ -31,7 +41,7 @@ class ShippingCrudTest extends TestCase
         ];
 
         // Create a new user using POST request
-        $response = $this->postJson('/api/shipping', $shippingData);
+        $response = $this->postJson(route('shipping.store'), $shippingData , $headers);
 
         // Assert the user is created and the response is successful
         $response->assertStatus(201); // 201 Created
@@ -42,26 +52,39 @@ class ShippingCrudTest extends TestCase
 
     /** @test */
     public function it_can_read_a_shipping_info(){
-        User::factory()->create();
-        Order::factory()->create();
+        Artisan::call('app:admin-api-token');
+        $user = User::factory()->create();
+        $order = Order::factory()->create();
         $shipping = Shipping::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
+        $headers = [
+            'GLOBAL-API-KEY' => $token,
+            'USER-API-KEY' => $user->api_token,
+        ];
 
-        $response = $this->getJson('/api/shipping/' . $shipping->id);
+        $response = $this->getJson(route('shipping.show', $shipping->id), $headers);
         $response->assertStatus(200); 
         $response->assertJson([
             'id' => $shipping->id,
             'email' => $shipping->email,
         ]);
 
-        $response = $this->getJson('/api/shipping/' . $shipping->id+1);
+        $response = $this->getJson(route('shipping.show', $shipping->id+1), $headers);
         $response->assertStatus(404); 
     }
 
     /** @test */
     public function it_can_update_a_shipping_info(){
-        User::factory()->create();
-        Order::factory()->create();
+        Artisan::call('app:admin-api-token');
+        $user = User::factory()->create();
+        $order = Order::factory()->create();
         $shipping = Shipping::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
+
+        $headers = [
+            'GLOBAL-API-KEY' => $token,
+            'USER-API-KEY' => $user->api_token,
+        ];
 
         $updatedData = [
             'name' => $this->faker->name,  
@@ -73,30 +96,38 @@ class ShippingCrudTest extends TestCase
             'phone' => $this->faker->phoneNumber, 
         ];
 
-        $response = $this->putJson('/api/shipping/' . $shipping->id, $updatedData);
+        $response = $this->putJson(route('shipping.update', $shipping->id),$updatedData ,$headers);
+
         $response->assertStatus(200); 
         $this->assertDatabaseHas('shippings', [
             'name' => $updatedData['name'],
             'email' => $updatedData['email'],
         ]);
 
-        $response = $this->putJson('/api/shipping/' . $shipping->id+1, $updatedData);
+        $response = $this->putJson(route('shipping.update', $shipping->id+100),$updatedData ,$headers);
         $response->assertStatus(404); 
     }
-
+    
     /** @test */
     public function it_can_delete_a_shipping_info(){
-        User::factory()->create();
-        Order::factory()->create();
+        Artisan::call('app:admin-api-token');
+        $user = User::factory()->create();
+        $order = Order::factory()->create();
+        $admin = Admin::factory()->create();
         $shipping = Shipping::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
 
-        $response = $this->deleteJson('/api/shipping/' . $shipping->id);
+        $headers = [
+            'GLOBAL-API-KEY' => $token,
+            'USER-API-KEY' => $user->api_token,
+        ];
+        $response = $this->deleteJson(route('shipping.destroy', $shipping->id),[], $headers);
         $response->assertStatus(200);
         $this->assertDatabaseMissing('shippings', [
             'id' => $shipping->id,
         ]);
 
-        $response = $this->deleteJson('/api/shipping/' . $shipping->id+1);
+        $response = $this->deleteJson(route('shipping.destroy', $shipping->id+100),[], $headers);
         $response->assertStatus(404);
     }
 }
