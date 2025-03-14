19459ee2b951a451217310feed7056244fe944d9
Kyle Metivier <metivier.kyle@yahoo.com>
2025-03-14 16:59:13 -0500

    Update ProductCrudTest.php
    clean up, added headers, and for other small best practices.

--- tests/Feature/ProductCrudTest.php
+++ tests/Feature/ProductCrudTest.php
@@ -3,10 +3,12 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Str;
 use App\Models\Product;
 use App\Models\Category;
+use App\Models\Admin;
+use App\Models\ApiToken;
 use Tests\TestCase;
 
 class ProductCrudTest extends TestCase
@@ -15,21 +17,28 @@ class ProductCrudTest extends TestCase
 
     /** @test */
     public function it_can_create_an_product(){
+        Artisan::call('app:admin-api-token');
         $category = Category::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
+        $admin = Admin::factory()->create();
+
+        $headers = [
+            'GLOBAL_API_KEY' => $token,
+            "USER_API_KEY" => $admin->api_token
+        ];
 
         $productData = [
             'name' => Str::random(10),
-            'price' => 5000,
+            'price' => rand(300, 10000),
             'short_description' => Str::random(25),
             'long_description' => Str::random(25),
-            'product_type_id' => $category->id,
+            'category_id' => $category->id,
             'featured' => (bool) rand(0, 1),
             'available' => (bool) rand(0, 1), 
         ];
 
-        $response = $this->postJson('/api/product', $productData);
-
-        $response->assertStatus(201);
+        $response = $this->postJson(route('product.create'), $productData, $headers);
+        $response->assertStatus(200);
         $this->assertDatabaseHas('products', [
             'name' => $productData['name'],
         ]);
@@ -37,10 +46,19 @@ class ProductCrudTest extends TestCase
     
     /** @test */
     public function it_can_read_a_product(){
-        ProductType::factory()->create();
+        Artisan::call('app:admin-api-token');
+        Category::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
+        $admin = Admin::factory()->create();
         $product = Product::factory()->create();
 
-        $response = $this->getJson('/api/product/' . $product->id);
+        $headers = [
+            'GLOBAL_API_KEY' => $token,
+            "USER_API_KEY" => $admin->api_token
+        ];
+
+        $response = $this->getJson(route('product.show', $product->id), $headers);
+
         $response->assertStatus(200);
         $this->assertDatabaseHas('products', [
             'id' => $product->id,
@@ -52,20 +70,28 @@ class ProductCrudTest extends TestCase
 
     /** @test */
     public function it_can_update_a_product(){
+        Artisan::call('app:admin-api-token');
         $category = Category::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
+        $admin = Admin::factory()->create();
         $product = Product::factory()->create();
+
+        $headers = [
+            'GLOBAL_API_KEY' => $token,
+            "USER_API_KEY" => $admin->api_token
+        ];
 
         $updatedData = [
             'name' => Str::random(10),
             'price' => 5000,
             'short_description' => Str::random(25),
             'long_description' => Str::random(25),
-            'product_type_id' => $category->id,
+            'category_id' => $category->id,
             'featured' => (bool) rand(0, 1),
             'available' => (bool) rand(0, 1), 
         ];
         
-        $response = $this->putJson('/api/product/' . $product->id, $updatedData);
+        $response = $this->putJson(route('product.update', $product->id), $updatedData ,  $headers);
         $response->assertStatus(200);
         $this->assertDatabaseHas('products', [
             'name' => $updatedData['name'],
@@ -77,40 +103,62 @@ class ProductCrudTest extends TestCase
 
     /** @test */
     public function it_can_delete_a_product(){
+        Artisan::call('app:admin-api-token');
         Category::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
+        $admin = Admin::factory()->create();
         $product = Product::factory()->create();
 
-        $response = $this->deleteJson('/api/product/' . $product->id);
+        $headers = [
+            'GLOBAL_API_KEY' => $token,
+            'USER_API_KEY' => $admin->api_token
+        ];
+
+        $response = $this->deleteJson(route('product.destroy', $product->id),[], $headers);
+
         $response->assertStatus(200);
         $this->assertDatabaseMissing('products', [
             'id' => $product->id,
         ]);
 
-        $response = $this->deleteJson('/api/product/' . $product->id+1);
+        $response = $this->deleteJson(route('product.destroy', $product->id+100),[], $headers);
         $response->assertStatus(404);
     }
 
     /** @test */
     public function can_display_avaliable_products(){
-        $response = $this->getJson('/api/product/available');
-        $response->assertStatus(404); 
-
+        Artisan::call('app:admin-api-token');
         Category::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
+        $admin = Admin::factory()->create();
         Product::factory()->create();
 
-        $response = $this->getJson('/api/product/available');
+        $headers = [
+            'GLOBAL_API_KEY' => $token,
+            'USER_API_KEY' => $admin->api_token
+        ];
+
+        $response = $this->getJson(route('product.avaliable'), $headers);
         $response->assertStatus(200); 
+
+        $response = $this->getJson('/api/product/available');
+        $response->assertStatus(404); 
     }
 
     /** @test */
     public function can_display_featured_products(){
-        $response = $this->getJson('/api/product/featured');
-        $response->assertStatus(404); 
-
+        Artisan::call('app:admin-api-token');
         Category::factory()->create();
+        $token = ApiToken::pluck('api_token')->first();
+        $admin = Admin::factory()->create();
         Product::factory()->create();
 
-        $response = $this->getJson('/api/product/featured');
+        $headers = [
+            'GLOBAL_API_KEY' => $token,
+            'USER_API_KEY' => $admin->api_token
+        ];
+
+        $response = $this->getJson(route('product.featured'), $headers);
         $response->assertStatus(200); 
     }
 }
